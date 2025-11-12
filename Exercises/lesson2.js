2c741e6d738591b1a9167e92eda3a6d51b36e181
Kenny Breckenridge <ksbreckenridge1@gmail.com>
2025-11-11 22:03:00 -0700

    Lesson 2 lab

---
+++ Exercises/lesson2.js
@@ -0,0 +1,16 @@
+const userName = 'Yo Mamma';
+let fruit = 'mango';
+fruit = 'Grape';
+
+// console.log(userName);
+// console.log(fruit);
+// console.log("Hello World");
+
+const numberOne = 1;
+const amount = 500;
+
+console.log(numberOne);
+console.log(amount);
+
+const FULL_NAME = "Deez Nuts";
+const AMOUNT = 1000
