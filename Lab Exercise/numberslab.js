c9ebdc9a860bf36c7ee14ddceda3fa704ce0f0fb
Kenny B <ksbreckenridge1@gmail.com>
2025-12-30 16:41:32 -0700

    Numbers labs for submission

---
+++ Lab Exercise/numberslab.js
@@ -0,0 +1,19 @@
+const numbVariable1 = 34_038;
+const numbVariable2 = 45_838.8734;
+const numbVariable3 = 0b011000101011;
+const numbVariable4 = 0x174C0;
+const numbVariable5 = -895_612;
+
+console.log(numbVariable1);
+console.log(numbVariable2);
+console.log(numbVariable3);
+console.log(numbVariable4);
+console.log(numbVariable5);
+
+const numHex = 0x1D;
+const numOct = 0o35;
+const numBin = 0b11101;
+
+console.log(numHex);
+console.log(numOct);
+console.log(numBin);
