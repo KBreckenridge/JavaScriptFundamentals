42fcea5715737b2962c2f566c8b7339175dc5fdd
Kenny B <ksbreckenridge1@gmail.com>
2026-01-24 00:05:23 -0700

    submit functions labs

---
+++ Lab Exercise/functionsLab.js
@@ -0,0 +1,14 @@
+function nonagonInfinity(paramOne, paramTwo = " Gizzard") {
+    return paramOne + paramTwo + "!"
+};
+console.log(nonagonInfinity("King"))
+
+function petroDragonicApocolypse(motor, spirit = 15){
+    return motor + spirit
+};
+console.log(petroDragonicApocolypse(1));
+console.log(petroDragonicApocolypse(1000000000));
+
+const organFarmer = (value) =>  value + 6;
+console.log(organFarmer(3))
+
